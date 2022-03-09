d6750a36214286be8b7979259773bc1e3c35da6c
vkumarmi <corp.virendra.mishra@gmail.com>
2022-03-09 11:20:38 +0530

    Changes

--- src/main/java/com/example/springboot/test1/Streamtest.java
+++ src/main/java/com/example/springboot/test1/Streamtest.java
@@ -2,8 +2,7 @@ package com.example.springboot.test1;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -11,8 +10,9 @@ public class Streamtest {
     public static void main(String[] args) {
         List<Employee> lst=new ArrayList<>();
         lst.add(new Employee(1,"virendra",36));
+        Set<Integer> sets=new HashSet<>();
        lst.add(new Employee(2,"dhirendra",25));
-
+        //Collections.EMPTY_LIST;
         System.out.println(lst.stream().map(employee -> employee.age).collect(Collectors.summingInt(number->number)));
     }
 
@@ -25,5 +25,18 @@ public class Streamtest {
         int id;
         String name;
         int age;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Employee employee = (Employee) o;
+            return id == employee.id && age == employee.age && Objects.equals(name, employee.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, age);
+        }
     }
 }
