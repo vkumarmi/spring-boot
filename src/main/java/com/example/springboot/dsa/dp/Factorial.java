1323da3caf237dd067bf51e2f5b9974805cd6c32
virendra <corp.virendra.mishra@gmail.com>
2022-05-29 00:10:04 +0530

    Updated according to java 8

--- src/main/java/com/example/springboot/dsa/dp/Factorial.java
+++ src/main/java/com/example/springboot/dsa/dp/Factorial.java
@@ -1,7 +1,7 @@
 package com.example.springboot.dsa.dp;
 
 public class Factorial {
-    public int factorial(int n){
+    public static long factorial(int n){
         if(n<=1){
             return 1;
         }
@@ -10,5 +10,25 @@ public class Factorial {
         }
     }
 
+    public static long factorialDPBU(int n){
+        long [] DP=new long[n];
+        DP[0]=1;
+        for(int i=2;i<=n;i++){
+            DP[i-1]=i*DP[i-2];
+        }
+        return DP[n-1];
+    }
+
+    public static long factorialDPTD(int n){
+
+    }
+
+    public static void main(String[] args) {
+        long start=System.currentTimeMillis();
+        System.out.println(factorial(20));
+        System.out.println("Time:"+(System.currentTimeMillis()-start));
+        System.out.println(factorialDPBU(20));
+
+    }
 
 }
---
+++ src/main/java/com/example/springboot/dsa/dp/UglyNumbers.java
@@ -0,0 +1,4 @@
+package com.example.springboot.dsa.dp;
+
+public class UglyNumbers {
+}
